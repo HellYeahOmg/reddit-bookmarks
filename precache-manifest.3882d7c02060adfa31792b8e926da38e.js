25fe700567de2523893ec243fdeff735ff6d4278
v.demidov@muz-lab.ru <16011202>
2020-06-03 14:43:48 +0300

    Updates

---
+++ precache-manifest.3882d7c02060adfa31792b8e926da38e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4891fdb729bb3644468228eb77512054",
+    "url": "/reddit-bookmarks/index.html"
+  },
+  {
+    "revision": "7c87519443264fa6896e",
+    "url": "/reddit-bookmarks/static/js/2.fcdc56e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reddit-bookmarks/static/js/2.fcdc56e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e1f0c8d0f6f0cde564",
+    "url": "/reddit-bookmarks/static/js/main.de6734f1.chunk.js"
+  },
+  {
+    "revision": "50cebb71cccfd686de3d",
+    "url": "/reddit-bookmarks/static/js/runtime-main.26a54792.js"
+  }
+]);
